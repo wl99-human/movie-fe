36bff03aa44f9b6149a2456734b261af4bf8fb5f
wl99-human <weileelim2012@gmail.com>
2023-08-27 21:57:59 +0800

    Remove unnecessary import in hero component

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,16 +7,18 @@ const Hero = ({movies}) => {
     <div className='movie-carousel-container'>
         <Carousel>
             {
-                movies.map((movie)=> {
+                movies?.map((movie)=> {
                     return (
                         <Paper>
                             <div className='movie-card-container'>
                                 <div className='movie-card'>
-                                    <div className='movie-poster'>
-                                        <img src={movie.poster} alt='' />
-                                    </div>
-                                    <div className='movie-title'>
-                                        <h4>{movie.title}</h4>
+                                    <div className='movie-detail'>
+                                        <div className='movie-poster'>
+                                            <img src={movie.poster} alt='' />
+                                        </div>
+                                        <div className='movie-title'>
+                                            <h4>{movie.title}</h4>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
